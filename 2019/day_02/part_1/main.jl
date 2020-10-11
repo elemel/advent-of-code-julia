6dc2990bb2e1a74c3197324b4e1454db113bbc73
Mikael Lind <elemel@elemel.se>
2020-10-11 16:48:49 +0200

    Extract Intcode module

--- 2019/day_02/part_1/main.jl
+++ 2019/day_02/part_1/main.jl
@@ -1,68 +1,13 @@
-using OffsetArrays
-
-module Opcodes
-
-const ADD = 1
-const MULTIPLY = 2
-const HALT = 99
-
-end # module Opcodes
-
-mutable struct Process
-    memory::OffsetArray{Int}
-    ip::Int
-
-    function Process(program::Array{Int}; ip::Int=0)
-        axis = 0:(length(program) - 1)
-        memory = OffsetArray(program, 0:(length(program) - 1))
-        new(memory, ip)
-    end
-end
-
-function halted(process)
-    process.memory[process.ip] == Opcodes.HALT
-end
-
-module Operations
-
-function add(process)
-    a = process.memory[process.ip + 1]
-    b = process.memory[process.ip + 2]
-    c = process.memory[process.ip + 3]
-
-    process.memory[c] = process.memory[a] + process.memory[b]
-    process.ip += 4
-end
-
-function multiply(process)
-    a = process.memory[process.ip + 1]
-    b = process.memory[process.ip + 2]
-    c = process.memory[process.ip + 3]
-
-    process.memory[c] = process.memory[a] * process.memory[b]
-    process.ip += 4
-end
-
-end # module Operations
-
-const OPCODE_TO_OPERATION = Dict(
-    Opcodes.ADD => Operations.add,
-    Opcodes.MULTIPLY => Operations.multiply,
-)
+include("../../Intcode.jl")
 
 function main()
     program = parse.(Int, split(read(stdin, String), ","))
-    process = Process(program)
+    process = Intcode.Process(program)
 
     process.memory[1] = 12
     process.memory[2] = 2
 
-    while !halted(process)
-        opcode = process.memory[process.ip]
-        operation = OPCODE_TO_OPERATION[opcode]
-        operation(process)
-    end
-
+    Intcode.run(process)
     println(process.memory[0])
 end
 
--- 2019/day_02/part_2/main.jl
+++ 2019/day_02/part_2/main.jl
@@ -1,74 +1,16 @@
-using OffsetArrays
-
-module Opcodes
-
-const ADD = 1
-const MULTIPLY = 2
-const HALT = 99
-
-end # module Opcodes
-
-mutable struct Process
-    memory::OffsetArray{Int}
-    ip::Int
-
-    function Process(program::Array{Int}; ip::Int=0)
-        axis = 0:(length(program) - 1)
-        memory = OffsetArray(copy(program), 0:(length(program) - 1))
-        new(memory, ip)
-    end
-end
-
-function halted(process)
-    process.memory[process.ip] == Opcodes.HALT
-end
-
-module Operations
-
-function add(process)
-    a = process.memory[process.ip + 1]
-    b = process.memory[process.ip + 2]
-    c = process.memory[process.ip + 3]
-
-    process.memory[c] = process.memory[a] + process.memory[b]
-    process.ip += 4
-end
-
-function multiply(process)
-    a = process.memory[process.ip + 1]
-    b = process.memory[process.ip + 2]
-    c = process.memory[process.ip + 3]
-
-    process.memory[c] = process.memory[a] * process.memory[b]
-    process.ip += 4
-end
-
-end # module Operations
-
-const OPCODE_TO_OPERATION = Dict(
-    Opcodes.ADD => Operations.add,
-    Opcodes.MULTIPLY => Operations.multiply,
-)
-
-function run(process)
-    while !halted(process)
-        opcode = process.memory[process.ip]
-        operation = OPCODE_TO_OPERATION[opcode]
-        operation(process)
-    end
-end
+include("../../Intcode.jl")
 
 function main()
     program = parse.(Int, split(read(stdin, String), ","))
 
     for noun = 0:99
         for verb = 0:99
-            process = Process(program)
+            process = Intcode.Process(program)
 
             process.memory[1] = noun
             process.memory[2] = verb
 
-            run(process)
+            Intcode.run(process)
 
             if process.memory[0] == 19690720
                 println(100 * noun + verb)
