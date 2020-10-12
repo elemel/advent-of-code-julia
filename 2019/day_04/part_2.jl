5b75b13ad607b881c81a2dafdd48ced5193a26a6
Mikael Lind <elemel@elemel.se>
2020-10-12 19:18:45 +0200

    Cleanup

--- 2019/day_04/part_2.jl
+++ 2019/day_04/part_2.jl
@@ -1,13 +1,13 @@
 using IterTools
  
 function is_valid_password(password)
-    groups = groupby(identity, string(password))
+    groups = groupby(identity, password)
 
-    if !any(length(group) == 2 for group in groups)
+    if !in(2, length.(groups))
         return false
     end
 
-    chars = split(string(password), "")
+    chars = split(password, "")
 
     if chars != sort(chars)
         return false
@@ -20,7 +20,9 @@ function main()
     first_password, last_password =
         parse.(Int, split(strip(read(stdin, String)), "-"))
 
-    println(count(is_valid_password, first_password:last_password))
+    println(count(
+        password -> is_valid_password(string(password)),
+        first_password:last_password))
 end
 
 main()
