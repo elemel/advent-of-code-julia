ee4b0ac82ee552475990f99a008822492faec9a5
Mikael Lind <elemel@elemel.se>
2020-12-05 22:33:01 +0100

    Rewrite day 5 of 2020

--- 2020/day_05/part_1.jl
+++ 2020/day_05/part_1.jl
@@ -1,8 +1,5 @@
 function decode_seat_id(s)
-    for kv in Dict("F" => "0", "B" => "1", "L" => "0", "R" => "1")
-        s = replace(s, kv)
-    end
-
+    s = join(c in "BR" ? "1" : "0" for c in s)
     parse(Int, s, base=2)
 end
 
--- 2020/day_05/part_2.jl
+++ 2020/day_05/part_2.jl
@@ -1,17 +1,12 @@
 function decode_seat_id(s)
-    for kv in Dict("F" => "0", "B" => "1", "L" => "0", "R" => "1")
-        s = replace(s, kv)
-    end
-
+    s = join(c in "BR" ? "1" : "0" for c in s)
     parse(Int, s, base=2)
 end
 
 function main()
     input = readlines(stdin)
-    seat_ids = Set([decode_seat_id(line) for line in input])
-    answer = first(
-        id for id in minimum(seat_ids):maximum(seat_ids)
-        if !(id in seat_ids) && id - 1 in seat_ids && id + 1 in seat_ids)
+    seat_ids = [decode_seat_id(line) for line in input]
+    answer = first(setdiff(minimum(seat_ids):maximum(seat_ids), seat_ids))
     println(answer)
 end
 
