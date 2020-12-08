7fbd935dc757d556583fbb953321b0a6d069c570
Mikael Lind <elemel@elemel.se>
2020-12-08 11:29:34 +0100

    Cleanup

--- 2020/day_08/part_2.jl
+++ 2020/day_08/part_2.jl
@@ -29,21 +29,32 @@ function run_program(instructions)
     return true, accumulator
 end
 
+function change_instruction(instruction)
+    operation, argument = instruction
+
+    if operation == "jmp"
+        return "nop", argument
+    elseif operation == "nop"
+        return "jmp", argument
+    else
+        return instruction
+    end
+end
+
 function main()
     input = readlines(stdin)
     instructions = parse_instruction.(input)
     answer = 0
 
     for address in 1:length(instructions)
-        operation, argument = instructions[address]
+        changed_instruction = change_instruction(instructions[address])
 
-        if operation == "jmp" || operation == "nop"
-            changed_operation = operation == "jmp" ? "nop" : "jmp"
+        if changed_instruction != instructions[address]
             changed_instructions = collect(instructions)
-            changed_instructions[address] = changed_operation, argument
-            completed, answer = run_program(changed_instructions)
+            changed_instructions[address] = changed_instruction
+            terminated_normally, answer = run_program(changed_instructions)
 
-            if completed
+            if terminated_normally
                 break
             end
         end
