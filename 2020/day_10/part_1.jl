ac61060eebe5ec818f88f9f33a04c14088815a09
Mikael Lind <elemel@elemel.se>
2020-12-11 10:19:32 +0100

    Cleanup

--- 2020/day_10/part_1.jl
+++ 2020/day_10/part_1.jl
@@ -1,18 +1,13 @@
 function main()
     input = readlines(stdin)
-    adapter_joltages = BitSet(parse.(Int, input))
-    input_joltages = union(BitSet([0]), adapter_joltages)
-    output_joltages = union(
-        adapter_joltages, BitSet([maximum(adapter_joltages) + 3]))
+    adapter_joltages = collect(BitSet(parse.(Int, input)))
     diff_counts = [0, 0, 0]
 
-    for o in output_joltages
-        for d in 1:3
-            if o - d in input_joltages
-                diff_counts[d] += 1
-                break
-            end
-        end
+    for (i, o) in zip(
+        [0; adapter_joltages],
+        [adapter_joltages; adapter_joltages[end] + 3])
+
+        diff_counts[o - i] += 1
     end
 
     answer = diff_counts[1] * diff_counts[3]
