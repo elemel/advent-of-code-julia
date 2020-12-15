1d3234fc7003ea0226c176d117a0596e4e2ec05b
Mikael Lind <elemel@elemel.se>
2020-12-15 07:06:12 +0100

    Solve day 15 of 2020

---
+++ 2020/day_15/part_2.jl
@@ -0,0 +1,23 @@
+function main()
+    input = readlines(stdin)
+    starting_numbers = parse.(Int, split(input[1], ","))
+    memory = Dict{Int, Int}()
+    answer = 0
+
+    for turn in 1:30000000
+        if turn <= length(starting_numbers)
+            number = starting_numbers[turn]
+        elseif !haskey(memory, answer)
+            number = 0
+        else
+            number = turn - 1 - memory[answer]
+        end
+
+        memory[answer] = turn - 1
+        answer = number
+    end
+
+    println(answer)
+end
+
+main()
